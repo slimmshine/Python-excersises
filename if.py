7c9cbb74236b10a28f314611bf6059cd03884275
slimmshine <slimmshine@gmail.com>
2017-10-11 17:07:38 +0300

    Create if.py

---
+++ if.py
@@ -0,0 +1,20 @@
+x = int(raw_input("Enter integer: "))
+if x < 0:
+	print "Negative"
+elif x == 0:
+	print 'Zero'
+else:
+	print 'Positive'
+
+
+list = [1,2,3,4,5,6,7,8,9]
+for i in list:
+	print i, "-pow2-", i**2
+
+words = ['cat', 'window', 'defenestrate']
+for w in words[:]:
+	if len(w) > 6:
+		words.insert(0,w)
+		print words
+
+
